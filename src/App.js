8c8fa6eae7a3efb3a8f04a73803c12b6aba0ffc7
Hugo Segura Alva <shugo.alva20@gmail.com>
2021-04-23 20:59:10 -0500

    modificando el proyecto en react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="card" >
+      <img src="https://i.ytimg.com/vi/xCGPPimXgJU/maxresdefault.jpg" className="card-img-top" alt="..."></img>
+      <div className="card-body">
+        <h5 className="card-title">Proyecto final</h5>
+        <p className="card-text">Aqui comienza, proyecto final con React JS y bootstrap</p>
+        <p>Probando GIT</p>
+        <a href="#" className="btn btn-primary">Comenzar</a>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
